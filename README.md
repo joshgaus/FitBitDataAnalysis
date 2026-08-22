10d56bf24cce2f076d2bd07f0bea23bb60592f87
Josh Gaus <jpegaus@gmail.com>
2026-08-22 18:04:05 -0400

    Code containing both custom df to mf for each csv and new function to do this

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ TODO
 1. Read all relevant data from files into main spreadsheet
    - ✔️sleep_score
    - ✔️daily_resting_heart_rate
-   - daily_respiratory_rate
+   - ✔️daily_respiratory_rate
    - daily_readiness
    - daily_oxygen_saturation
    - daily_heart_rate_variability
@@ -16,6 +16,8 @@ TODO
    - Daily Heart Rate Variability Summary
    - time_in_heart_rate_zones
    - sedentary_minutes
+   - Make function for reusability
+   - Read wake up time from sleep_score (right now its normalizing the timestamp to just the date)
 2. Visualize data
 3. Perform correlation analysis on data for insights
 4. Allow for use with web portal
--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-from datetime import datetime
-
-import zoneinfo
 from pathlib import Path
-from tabnanny import NannyNag
-
 import pandas as pd
-from pandas import DataFrame
 
 google_health_dir = Path(__file__).resolve().parent / "Google Health"
-relevant_csv_file_keywords = ["sleep_score",
-                  "daily_resting_heart_rate",
-                  "daily_respiratory_rate",
-                  "daily_readiness",
-                  "daily_oxygen_saturation",
-                  "daily_heart_rate_variability",
-                  "cardio_load_observed_interval",
-                  "cardio_acute_chronic_workload_ratio"]
-                 # "Daily SpO2 -"]
-                 # "Daily Respiratory Rate Summary"
-                 # "Daily Heart Rate Variability Summary",]
-relevant_json_file_keywords = ["time_in_heart_rate_zones",  # 1 .json file per day
-                  "sedentary_minutes-"]             # 1 .json file per month
 
 def read_csvs_to_dataframe():
+
     # main_data is the primary dataframe which all other data is imported into
     main_frame = pd.DataFrame()
 
@@ -35,7 +17,7 @@ def read_csvs_to_dataframe():
 
     main_frame = pd.concat([main_frame, df])
     main_frame["timestamp"] = pd.to_datetime(main_frame["timestamp"], format="ISO8601")
-
+    '''
     # sleep_score.csv
     # Reads sleep_score.csv's relevant columns, formats the timestamp
 
@@ -44,7 +26,6 @@ def read_csvs_to_dataframe():
     main_frame["deep sleep in minutes"] = None
     main_frame["restlessness"] = None
 
-    sleep_score_csv_path = google_health_dir / "Sleep Score" / "sleep_score.csv"
     # Relevant columns: 1-timestamp, 2-overall_score, 6-deep_sleep_in_minutes, 8-restlessness
     df = pd.read_csv(sleep_score_csv_path, usecols=[1,2,6,8],parse_dates=True)
     df["timestamp"] = pd.to_datetime(df["timestamp"], format="ISO8601").dt.normalize()
@@ -56,5 +37,51 @@ def read_csvs_to_dataframe():
                 main_frame.at[mf_index, "overall sleep score"] = df.at[df_index, "overall_score"]
                 main_frame.at[mf_index, "deep sleep in minutes"] = df.at[df_index, "deep_sleep_in_minutes"]
                 main_frame.at[mf_index, "restlessness"] = df.at[df_index, "restlessness"]
+    '''
+    sleep_score_csv_path = google_health_dir / "Sleep Score" / "sleep_score.csv"
+    daily_respiratory_rate_csv_path = google_health_dir / "Physical Activity_GoogleData" / "daily_respiratory_rate.csv"
+    main_frame = read_csv_into_main_frame(main_frame, sleep_score_csv_path, [1,2,6,8])
+    main_frame = read_csv_into_main_frame(main_frame, daily_respiratory_rate_csv_path, [0,1])
+    print(main_frame.to_string())
+'''
+    # daily_respiratory_rate.csv
+    # Reads daily_respiratory_rate.csv's relevant columns, formats timestamp
+
+    # Initialize rows to be filled in main_frame from daily_respiratory_rate.csv
+    main_frame["daily resp. rate, breaths/min"] = None
+
+    daily_respiratory_rate_csv_path = google_health_dir / "Physical Activity_GoogleData" / "daily_respiratory_rate.csv"
+    # Relevant columns: 0-timestamp, 1-breaths per minute
+    df = pd.read_csv(daily_respiratory_rate_csv_path, usecols=[0,1], parse_dates=True)
+    df["timestamp"] = pd.to_datetime(df["timestamp"], format="ISO8601")
+
+    # Reconcile relevant rows in daily_respiratory_rate.csv and main dataframe
+    for mf_index, mf_timestamp in enumerate(main_frame["timestamp"]):
+        for df_index, df_timestamp in enumerate(df["timestamp"]):
+            if df_timestamp == mf_timestamp:
+                main_frame.at[mf_index, "daily resp. rate, breaths/min"] = df.at[df_index, "breaths per minute"]
+
+    print(main_frame.to_string())
+'''
+def read_csv_into_main_frame(mf: pd.DataFrame, csv_path: Path, col_nums_for_df: list[int]) -> pd.DataFrame:
+
+    # Read relevant data from csv into df
+    df = pd.read_csv(csv_path, usecols=col_nums_for_df, parse_dates=True)
+    df["timestamp"] = pd.to_datetime(df["timestamp"], format="ISO8601").dt.normalize()
+
+    # Initialize empty columns in main_frame to import data from df into
+    col_names = []
+    for col_name in df.columns.tolist():
+        if col_name != "timestamp":
+            mf[col_name] = None
+            col_names.append(col_name)
+
+    # Reconcile relevant rows in csv and main_frame
+    for mf_index, mf_timestamp in enumerate(mf["timestamp"]):
+        for df_index, df_timestamp in enumerate(df["timestamp"]):
+            if df_timestamp == mf_timestamp:
+                for column in col_names:
+                    mf.at[mf_index, column] = df.at[df_index, column]
+    return mf
 
 read_csvs_to_dataframe()
